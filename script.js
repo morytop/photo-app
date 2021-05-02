94aac5a81edb237b252932af015ae049c4f42055
Piotr Moryto <piotrmoryto@gmail.com>
2021-05-02 17:04:29 +0200

    add RGB effects

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const context = canvas.getContext("2d");
 const reader = new FileReader();
 const img = new Image();
 
-img.src = "planet-1702788_1920.jpg";
-img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    context.drawImage(img, 0, 0);
-};
-
 function uploadImage(e) {
     reader.onload = () => {
         img.onload = () => {
@@ -59,8 +52,58 @@ const invert = () => {
     context.putImageData(imageData, 0, 0);
 };
 
+function rbg() {
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        const green = data[i + 1];
+        data[i] = data[i];
+        data[i + 1] = data[i + 2];
+        data[i + 2] = green;
+    }
+    context.putImageData(imageData, 0, 0);
+}
 
-imageLoader.addEventListener("change", uploadImage);
+function bgr() {
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        const red = data[i];
+        data[i] = data[i + 2];
+        data[i + 1] = data[i + 1];
+        data[i + 2] = red;
+    }
+    context.putImageData(imageData, 0, 0);
+}
+
+function gbr() {
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        const red = data[i];
+        data[i] = data[i + 1];
+        data[i + 1] = data[i + 2];
+        data[i + 2] = red;
+    }
+    context.putImageData(imageData, 0, 0);
+}
+
+function grb() {
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        const red = data[i];
+        data[i] = data[i + 1];
+        data[i + 1] = red;
+        data[i + 2] = data[i + 2];
+    }
+    context.putImageData(imageData, 0, 0);
+}
+
 document.querySelectorAll("button")[0].addEventListener("click", greyscale);
 document.querySelectorAll("button")[1].addEventListener("click", sepia);
 document.querySelectorAll("button")[2].addEventListener("click", invert);
+document.querySelectorAll("button")[3].addEventListener("click", rbg);
+document.querySelectorAll("button")[4].addEventListener("click", bgr);
+document.querySelectorAll("button")[5].addEventListener("click", gbr);
+document.querySelectorAll("button")[6].addEventListener("click", grb);
