3060ce7f3556fc287b409d81538e92ff5575b649
Piotr Moryto <piotrmoryto@gmail.com>
2021-05-02 16:25:12 +0200

    add download images

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-
+const reader = new FileReader()
 const img = new Image();
+
 img.src = "planet-1702788_1920.jpg";
 img.onload = () => {
     canvas.width = img.width;
     canvas.height = imp.height;
     context.drawImage(img, 0, 0);
 };
+
+function uploadImage(e) {
+    reader.onload = () => {
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        };
+        img.src = reader.result;
+    };
+    reader.readAsDataURL(e.target.files[0]);
+}
+
+const imageLoader = document.getElementById('uploader');
+imageLoader.addEventListener("change", uploadImage)
